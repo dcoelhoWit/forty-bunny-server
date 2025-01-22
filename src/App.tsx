1d1f6e767cfc24a18d72fb784be06c1ca624af4f
Diogo Coelho <diogo.coelho@wit-software.com>
2025-01-22 16:47:38 +0000

    Bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getDatabase, ref, onValue } from "firebase/database";
 import firebaseAppConf from "./firebase";
-import { parseQuestions, parseVotesQuestions } from "./utils/Parser"
+import { parseQuestions, parseVotesQuestions } from "./utils/Parser";
 import QuestionPage from "./components/QuestionPage";
 import StartPage from "./components/StartPage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowResultsPage from "./components/ShowResultsPage";
 import ResultsPage from "./components/ResultsPage";
+import { setActiveQuestion } from "./utils/FirebaseSetter";
 
 // Styles
 const AppContainer = styled.div`
@@ -21,7 +18,7 @@ const AppContainer = styled.div`
   font-family: "Roboto", sans-serif;
   color: #fff;
 `;
-const Container = styled.div.attrs({ className: "container"})`
+const Container = styled.div.attrs({ className: "container" })`
   max-width: 1200px;
   margin: 0 auto;
 `;
@@ -30,48 +27,38 @@ function App() {
   const [questions, setQuestions] = useState<any[]>([]);
   const [results, setResults] = useState<any[]>([]);
 
+  // Set Default Active Question: None
   useEffect(() => {
-    // Initialize the Firebase database with the provided configuration
-    const database = getDatabase(firebaseAppConf);
+    setActiveQuestion("");
+  });
 
-    // Reference to the specific collection in the database
+  // Fetch Questions List
+  useEffect(() => {
+    const database = getDatabase(firebaseAppConf);
     const collectionRef = ref(database, "Questions");
 
-    // Function to fetch data from the database
     const fetchData = () => {
-      // Listen for changes in the collection
       onValue(collectionRef, (snapshot) => {
         const dataItem = snapshot.val();
-
-        // Check if dataItem exists
         if (dataItem) {
-          // Convert the object values into an array
           const parsedQuestions = parseQuestions(dataItem);
           setQuestions(parsedQuestions);
         }
       });
     };
 
-    // Fetch data when the component mounts
     fetchData();
   }, []);
 
+  // Fetch Answers
   useEffect(() => {
-    // Initialize the Firebase database with the provided configuration
     const database = getDatabase(firebaseAppConf);
-
-    // Reference to the specific collection in the database
     const collectionRef = ref(database, "Votes");
 
-    // Function to fetch data from the database
     const fetchData = () => {
-      // Listen for changes in the collection
       onValue(collectionRef, (snapshot) => {
         const dataItem = snapshot.val();
-
-        // Check if dataItem exists
         if (dataItem) {
-          // Convert the object values into an array
           const parsedQuestions = parseVotesQuestions(dataItem);
           console.log();
           setResults(parsedQuestions);
@@ -79,7 +66,6 @@ function App() {
       });
     };
 
-    // Fetch data when the component mounts
     fetchData();
   }, []);
 
@@ -93,8 +79,14 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<StartPage></StartPage>}></Route>
-            <Route path="/results" element={<ShowResultsPage></ShowResultsPage>}></Route>
-            <Route path="/chart" element={<ResultsPage votesQuestions= {results}></ResultsPage>}></Route>
+            <Route
+              path="/results"
+              element={<ShowResultsPage></ShowResultsPage>}
+            ></Route>
+            <Route
+              path="/chart"
+              element={<ResultsPage votesQuestions={results}></ResultsPage>}
+            ></Route>
             {questions.map((question) => (
               <Route
                 path={getPath(question.index)}
@@ -107,7 +99,5 @@ function App() {
     </AppContainer>
   );
 }
-
- 
 
 export default App;
